25aef4635d0e978c1a304305aeb016410c597611
Steve Johnson <sjohnson@yelp.com>
2011-11-11 15:29:50 -0800

    Added one last attr to JobStatus to know when the job is actally complete and the data is available

--- mrjob/runner.py
+++ mrjob/runner.py
@@ -128,6 +128,13 @@ class JobStatus(object):
         #: CANCELLED, PENDING, WAITING, RUNNING, UNKNOWN
         self.state = 'UNKNOWN'
 
+        self.counters = []
+
+        #: True if all job actions have been completed. Distinct from
+        #: in_progress because it encompasses streaming the output in addition
+        #: to just finishing processing
+        self.complete = False
+
     def generate_status_message(self):
         """Add a status message based on job status attributes"""
         if self.running_step_name:
@@ -147,9 +154,6 @@ class JobStatus(object):
 
 
     def as_dict(self):
-        attrs = ('in_progress', 'success', 'status_string', 'error_string',
-                 'last_state_change_reason', 'state', 'time_updated',
-                 'running_time', 'step_nums')
         return dict(
             in_progress=self.in_progress,
             success=self.success,
@@ -159,7 +163,9 @@ class JobStatus(object):
             state=self.state,
             time_updated=str(self.time_updated),
             running_time=self.running_time,
-            step_nums=self.step_nums
+            step_nums=self.step_nums,
+            counters=self.counters,
+            complete=self.complete,
         )
 
     def __setattr__(self, attribute, value):
@@ -605,6 +611,7 @@ class MRJobRunner(object):
         self._ran_job = True
 
     def update_status(self, new_status):
+        new_status.counters = self.counters()
         self.job_status = new_status
         if self.job_status_file_path is not None:
             with open(self.job_status_file_path, 'w') as f:
@@ -695,6 +702,9 @@ class MRJobRunner(object):
 
         if mode_has('ALL', 'LOGS', 'IF_SUCCESSFUL'):
             self._cleanup_logs()
+
+        self.job_status.complete = True
+        self.update_status(self.job_status)
 
     def counters(self):
         """Get counters associated with this run in this form:
